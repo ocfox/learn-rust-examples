c69b9e8f86367f90f6c4b88fc4efe23ed2554186
ocfox <ocfox@outlook.com>
2021-10-02 19:25:13 +0800

    5

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,20 @@
-use std::fmt; // 导入 `fmt` 模块。
-
-// 定义一个包含单个 `Vec` 的结构体 `List`。
-struct List(Vec<i32>);
-
-impl fmt::Display for List {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // 使用元组的下标获取值，并创建一个 `vec` 的引用。
-        let vec = &self.0;
-
-        write!(f, "[")?;
-
-        // 使用 `v` 对 `vec` 进行迭代，并用 `count` 记录迭代次数。
-        for (count, v) in vec.iter().enumerate() {
-            // 对每个元素（第一个元素除外）加上逗号。
-            // 使用 `?` 或 `try!` 来返回错误。
-            if count != 0 {
-                write!(f, ", ")?;
-            }
-            write!(f, "{}: {}", count, v)?;
-        }
-
-        // 加上配对中括号，并返回一个 fmt::Result 值。
-        write!(f, "]")
-    }
-}
+use std::time::Instant;
 
 fn main() {
-    let v = List(vec![1, 2, 3]);
-    println!("{}", v);
+    let start = Instant::now();
+    let mut t: i32 = 0;
+    for i in 100..200 {
+        for tt in 2..i {
+            if i % tt == 0 {
+                t = tt;
+                break;
+            }
+        }
+        if t == i {
+            print!("{} ", t);
+        }
+    }
+
+    let end = start.elapsed();
+    println!("use ss {:?}", end);
 }
